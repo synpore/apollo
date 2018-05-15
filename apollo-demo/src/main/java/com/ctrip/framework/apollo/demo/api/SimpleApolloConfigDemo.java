edb1f99f23b00c4a7847a20687c4baa83141f190
hurenxian <hurenxian@iqianjin.com>
2018-05-15 19:42:51 +0800

    支持json格式配置测试

--- apollo-demo/src/main/java/com/ctrip/framework/apollo/demo/api/SimpleApolloConfigDemo.java
+++ apollo-demo/src/main/java/com/ctrip/framework/apollo/demo/api/SimpleApolloConfigDemo.java
@@ -1,19 +1,19 @@
 package com.ctrip.framework.apollo.demo.api;
 
-import com.google.common.base.Charsets;
-
 import com.ctrip.framework.apollo.Config;
 import com.ctrip.framework.apollo.ConfigChangeListener;
 import com.ctrip.framework.apollo.ConfigService;
 import com.ctrip.framework.apollo.model.ConfigChange;
 import com.ctrip.framework.apollo.model.ConfigChangeEvent;
-
+import com.google.common.base.Charsets;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 /**
  * @author Jason Song(song_s@ctrip.com)
@@ -22,6 +22,8 @@ public class SimpleApolloConfigDemo {
   private static final Logger logger = LoggerFactory.getLogger(SimpleApolloConfigDemo.class);
   private String DEFAULT_VALUE = "undefined";
   private Config config;
+  private  Config jsonConfig;
+  private static final Gson gson = new Gson();
 
   public SimpleApolloConfigDemo() {
     ConfigChangeListener changeListener = new ConfigChangeListener() {
@@ -37,12 +39,24 @@ public class SimpleApolloConfigDemo {
       }
     };
     config = ConfigService.getAppConfig();
+    //测试json格式的配置
+    jsonConfig=ConfigService.getConfig("JsonNameSpace.json");
     config.addChangeListener(changeListener);
+    jsonConfig.addChangeListener(changeListener);
   }
 
   private String getConfig(String key) {
     String result = config.getProperty(key, DEFAULT_VALUE);
     logger.info(String.format("Loading key : %s with value: %s", key, result));
+    return result;
+  }
+
+  //获取json配置中的属性
+  private String getJsonConfig(String key) {
+    String jsonString = jsonConfig.getProperty("content",DEFAULT_VALUE);
+    Map<String,String> configMap=gson.fromJson(jsonString, Map.class);
+    String result=configMap.get(key);
+    logger.info(String.format("Loading json key : %s with value: %s", key, result));
     return result;
   }
 
@@ -61,6 +75,7 @@ public class SimpleApolloConfigDemo {
         System.exit(0);
       }
       apolloConfigDemo.getConfig(input);
+      apolloConfigDemo.getJsonConfig(input);
     }
   }
 }
